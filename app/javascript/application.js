0dcd3a332ff033dbef1f3a3d954a60807e5c0c23
trailsofink <trailsofink@gmail.com>
2025-08-07 20:26:19 -0500

    fizzbuZZZZ

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,25 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 
 // Change to true to allow Turbo
-Turbo.session.drive = false
+Turbo.session.drive = false;
 
 // Allow UJS alongside Turbo
 import jquery from "jquery";
 window.jQuery = jquery;
 window.$ = jquery;
-import Rails from "@rails/ujs"
+import Rails from "@rails/ujs";
 Rails.start();
+
+for (let n = 1; n <= 100; n++) {
+  if (n % 15 === 0) {
+    console.log("fizzbuzz");
+  } else if (n % 5 === 0) {
+    console.log("buzz");
+  } else if (n % 3 === 0) {
+    console.log("fizz");
+  } else {
+    console.log(n);
+  }
+}
